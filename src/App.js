c56cb823b2b1289663f59840f91b3637b29a9b2c
laetitia piat <laeptitchat29@gmail.com>
2023-04-17 16:18:17 +0200

    modif

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import AppRoutes from "./AppRoutes";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header.js";
 
 const App = () => {
   return <AppRoutes />;
