f5bb8c46b473887f94173316054d7d9994370d95
laetitia piat <laeptitchat29@gmail.com>
2023-04-17 15:06:59 +0200

    suppression composant error

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,17 @@
 import React from "react";
-import "../components/Error/error.css";
-import BodyError from "../components/Error/BodyError";
+import "../styles/error.css";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <BodyError />;
+  return (
+    <div className="bodyError">
+      <p className="textError">404 </p>
+      <p className="oups">Oups! La page que vous demandez n'existe pas.</p>
+      <p className="lien">
+        <Link to="/">Retourner sur la page d'accueil</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Error;
